0f1b4dd6a78b9c41b19882b52bcedeb436611cb8
kin fuyuki <jktechsoftwares@gmail.com>
2025-07-31 00:00:44 -0300

    fixed model struct

--- src/game/world/world.cpp
+++ src/game/world/world.cpp
@@ -1,26 +1,41 @@
 #include "world.h"
 
 
+
+model cube;
 map::map() {}
 map::map(int width, int height) : pixelbuffer(width, height,0) {
+	cube.vertices= new vec3[8]
+	{
+		{0,0,0},
+		{1,0,0},
+		{0,1,0},
+		{1,1,0},
+		{0,0,1},
+		{1,0,1},
+		{0,1,1},
+		{1,1,1}
+	};
+	cube.faces= new Triangle[12]
+	{
+		{0,1,2},
+		{1,3,2},
+		{4,5,6},
+		{5,7,6},
+		{0,1,4},
+		{1,5,4},
+		{2,3,6},
+		{3,7,6},
+		{0,2,4},
+		{2,6,4},
+		{1,3,5},
+		{3,7,5}
+	};
 }
 map::~map() {}
 
 
-
-
-Triangle triangle(vec3(0,0,0), vec3(1,0,0), vec3(0,1,0));
 void map::render(Window* window) {
 	SDL_UpdateTexture(window->framebuffer, nullptr, pixelbuffer.data, pixelbuffer.width*4);
 	SDL_RenderCopy(window->renderer, window->framebuffer, nullptr, nullptr);
-	short pos = 100;
-	short idx=100;
-	for (short x = 0; x < 100; x++) {
-		for (short y = 0; y < x; y++) {
-			short px = x + pos;
-			short py = y + pos;
-			
-			this->pixelbuffer.data[py * 380 + px] = { 255,255,255,255 };
-		}
-	}
 }
