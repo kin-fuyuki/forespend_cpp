2245ea2d1a6ea05d466a3734054bdebb7eadff40
kin fuyuki <jktechsoftwares@gmail.com>
2025-09-14 19:23:09 -0300

    Clean repo and add .gitignore

--- CMakeFiles/l5.64.dir/src/game/world/biome.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/l5.64.dir/src/game/world/biome.cpp.o: \
- /home/komi/src/cpp/forespend_cpp/src/game/world/biome.cpp \
- /usr/include/stdc-predef.h \
- /home/komi/src/cpp/forespend_cpp/src/game/world/biome.h
--- CMakeFiles/l5.64.dir/src/game/world/building.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/l5.64.dir/src/game/world/building.cpp.o: \
- /home/komi/src/cpp/forespend_cpp/src/game/world/building.cpp \
- /usr/include/stdc-predef.h \
- /home/komi/src/cpp/forespend_cpp/src/game/world/building.h
